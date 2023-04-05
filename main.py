e66452ac704fef3273fc161d3de8b9150f74069a
Daniel Suárez <dwsuarez98@gmail.com>
2023-04-04 22:48:01 -0300

    README

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from scraping.scrap_dollar import ScrapDollar
 from scraping.scrap_i_financieros import ScrapIndices
-from scraping.urls import *
+#from scraping.urls import *
 
 
 class Menu:
+
     def __init__(self):
         self.dollar = ScrapDollar()
         self.indices = ScrapIndices()
-
 
     def run(self):
         while True:
             print("Menu")
             print("1 Valores actuales del dóllar")
             print("2 Indices financieros")
-
 
             opcion = input("Seleccione la opción: ")
 
@@ -28,10 +27,11 @@ class Menu:
                 print("Programa finalizado")
                 break
 
-
+            else:
+                print("Por favor elija una opción de la lista")
+                
 
 
 if __name__ == '__main__':
     menu = Menu()
     menu.run()
-    
